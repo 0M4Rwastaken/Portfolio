b0a9c02a2e792c573e2ecebcc61f388f093f34aa
0M4Rwastaken <bahomar0@gmail.com>
2025-01-14 23:08:19 -0500

    Website Update

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ function toggleMenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+     
 }
+
+function toggleNight() {
+
+    const night = document.body.classList.toggle('night-mode');
+    night.classList("open")
+
+    
+}
+
+
+ 
