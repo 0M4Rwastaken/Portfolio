d4d8ca38b9e25a46aed11ccfb64c2d5931f55d10
Omar Bah <o@Omars-MacBook-Air.local>
2026-03-04 14:18:10 -0500

    Updated pictures / alignment

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+     
+}
+
+function toggleNight() {
+    const body = document.body;
+    const nightModeButton = document.querySelector('.night-mode');
+    const nightModeIcon = nightModeButton.querySelector('img');
+
+    body.classList.toggle('night-mode');
+
+    // Update button icon based on current mode
+    if (body.classList.contains('night-mode')) {
+        nightModeIcon.src = './assets/night-mode-btn.png'; // Icon for light mode
+    } else {
+        nightModeIcon.src = './assets/night-mode.png'; // Icon for dark mode
+    }
+}
+
+
+ 
